9f4cd72689098141db48358084b2b1be4e5d47dc
masaki moriyama <moriyamasaki211@gmail.com>
2017-10-31 10:04:40 +0000

    added index and create actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,19 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    flash[:notice] = "タスクを登録しました。"
+    redirect_to root_path
+  end
+  
+  private
+  def task_params
+    # titleとcontentのみをパラメータとして受け取る
+    params.require(:task).permit(:title,:content)
   end
 end
